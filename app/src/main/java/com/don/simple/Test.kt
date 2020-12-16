9b11e39600948947aa0055be18d121a00df075c8
rendongwei <rendongwei@jinrongwealth.com>
2020-12-16 14:37:39 +0800

    修改

--- app/src/main/java/com/don/simple/Test.kt
+++ app/src/main/java/com/don/simple/Test.kt
@@ -1,5 +1,22 @@
 package com.don.simple
 
-fun main(){
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.runBlocking
 
+
+fun main() = runBlocking {
+    flow {
+        for (i in 0..3) {
+            emit(i)
+        }
+    }.onStart {
+        println("开始了")
+    }.onCompletion {
+        println("完成了")
+    }.collect {
+        println(it)
+    }
 }
---
+++ frame/src/main/java/com/don/frame/core/base/viewmodel/BaseViewModel.kt
@@ -0,0 +1,17 @@
+package com.don.frame.core.base.viewmodel
+
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+open class BaseViewModel : ViewModel(), LifecycleObserver {
+
+    fun launch(block: suspend CoroutineScope.() -> Unit) {
+        viewModelScope.launch {
+            block()
+        }
+    }
+
+}
